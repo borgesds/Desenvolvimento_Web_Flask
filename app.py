266e54357f14390e26563ad94e3c2f4dbfdd4cdb
André Borges <seuemail@example.com>
2023-08-22 20:40:04 -0300

    up Code

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+frutas = []
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def principal():
-    return render_template("index.html")
+    """
+    frutas = ['Morango', 'Banana', 'Abacaxi',
+              'Laranja', 'Manga', 'Maça', 'Melão',
+              'Jambo', 'Tangerina']
+    """
+
+    if request.method == 'POST':
+        if request.form.get('fruta'):
+            frutas.append(request.form.get('fruta'))
+
+    return render_template("index.html", frutas=frutas)
 
 
 @app.route('/sobre')
 def sobre():
-    return render_template("sobre.html")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    notas = {"Alan": 6.5, "Carlos": 5.5, "Marcos": 8.9,
+             "Camila": 7.8, "Roberto": 4.9, "Alexia": 7.2}
+    return render_template("sobre.html", notas=notas)
