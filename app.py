c04d9b1245b762675a95c38fe860f8cb6b23b470
André Borges <seuemail@example.com>
2023-08-22 21:03:02 -0300

    up Code

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 frutas = []
+registros = []
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,8 +21,16 @@ def principal():
     return render_template("index.html", frutas=frutas)
 
 
-@app.route('/sobre')
+@app.route('/sobre', methods=['GET', 'POST'])
 def sobre():
+    """
     notas = {"Alan": 6.5, "Carlos": 5.5, "Marcos": 8.9,
              "Camila": 7.8, "Roberto": 4.9, "Alexia": 7.2}
-    return render_template("sobre.html", notas=notas)
+    """
+
+    if request.method == 'POST':
+        if request.form.get('aluno') and request.form.get('nota'):
+            registros.append({"aluno": request.form.get('aluno'),
+                              "nota": request.form.get('nota')})
+
+    return render_template("sobre.html", registros=registros)
